97d59d496b6f52fb9480c2a1801b02dc797a8ed5
lon91ong <lon91ong@gmail.com>
2021-07-08 17:19:34 +0800

    Update diy-B70.sh

--- Others/B70/diy-B70.sh
+++ Others/B70/diy-B70.sh
@@ -21,9 +21,9 @@ find target/linux -path "target/linux/*/config-*" | xargs -i sed -i '$a CONFIG_A
 CONFIG_NR_CPUS=128\nCONFIG_FAT_DEFAULT_IOCHARSET="utf8"' {}
 
 #默认主题改为argon
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
-rm -rf package/feeds/custom/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/feeds/custom/luci-theme-argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+rm -rf feeds/custom/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/custom/luci-theme-argon
 
 # 机型名称适配
 sed -i 's/HC5962/&-SPI")\n\t\tname="hc5962-spi"\n\t\t;;\n\t\*"&/1' target/linux/ramips/base-files/lib/ramips.sh
@@ -46,8 +46,6 @@ sed -i 's/^[ \t]*//g' ./target/linux/ramips/image/mt7621.mk
 #sed -i 's/0xfb0000/0x1fa0000/g' target/linux/ramips/dts/HC5962-SPI.dts
 #sed -i 's/16064k/32384k/g' target/linux/ramips/image/mt7621.mk
 
-# 默认turboacc开启DNS Caching功能
-#sed -i -r "s/(dns_caching )'0'/\1'1'/1" package/feeds/custom/luci-app-turboacc/root/etc/config/turboacc 
 # 修复DHCP服务, 从5.4内核改回4.14内核的resolv.conf路径
 sed -i 's|resolv.conf.d/resolv.conf.auto|resolv.conf.auto|g' `grep -l resolv.conf.d package/feeds/custom/*/root/etc/init.d/*`
 
