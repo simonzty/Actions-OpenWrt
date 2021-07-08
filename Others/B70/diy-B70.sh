973b604b349108d33ebe57d94c21f413181b6cdc
lon91ong <lon91ong@gmail.com>
2021-07-08 17:17:10 +0800

    Update diy-B70.sh

--- Others/B70/diy-B70.sh
+++ Others/B70/diy-B70.sh
@@ -52,4 +52,4 @@ sed -i 's/^[ \t]*//g' ./target/linux/ramips/image/mt7621.mk
 sed -i 's|resolv.conf.d/resolv.conf.auto|resolv.conf.auto|g' `grep -l resolv.conf.d package/feeds/custom/*/root/etc/init.d/*`
 
 # custom插件汉化
-mv package/feeds/custom/luci-app-turboacc/po/zh_Hans package/feeds/custom/luci-app-turboacc/po/zh-cn
+mv feeds/custom/luci-app-turboacc/po/zh_Hans feeds/custom/luci-app-turboacc/po/zh-cn
